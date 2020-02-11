0e6ac90d1460286c666043ceb671f74a320a8c42
Reda NOUSHI <reda_noushi@yahoo.com>
2020-02-12 00:38:42 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,5 +4,6 @@ output "config" {
     region         = data.aws_region.current.name
     role_arn       = aws_iam_role.iam_role.arn
     dynamodb_table = aws_dynamodb_table.dynamodb_table.name
+    superconfig    = "constant"
   }
 }
